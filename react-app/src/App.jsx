9a7aee3487503331adddd81be5682a71ce28d6e7
Abdelrahman Elgendy <abdelelgendy11@gmail.com>
2025-08-28 17:37:51 -0400

    sue state for liking movie
    - when the like button is pressed it will switch to 'liked'
    - the hasliked state will be switched from false to true
    - if the movie is liked the onlick will auto toggle beetwen like and liked based on if its liked or not liked

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -2,16 +2,22 @@ import { useState } from 'react'
 import './App.css'
 
 const Card=({title})=>{
+
+  const [hasLiked, setHasLiked] =useState(false);
+
+
   return(
     <div className='card'>
       <h2>{title}</h2>
+
+      <button onClick={()=>setHasLiked(!hasLiked)}>
+       {hasLiked ? '❤️' : '🤍'} 
+      </button>
     </div>
   )
 }
 
 const App=()=>{
-
-  const [] =useState()
   
   return(
     <div className='card-container'>    
