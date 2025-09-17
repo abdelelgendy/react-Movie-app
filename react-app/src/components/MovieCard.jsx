4863895e5dbcc8137a26029867cc6730c6623ed5
Abdelrahman Elgendy <abdelelgendy11@gmail.com>
2025-09-16 23:59:53 -0400

    moviecard update

--- react-app/src/components/MovieCard.jsx
+++ react-app/src/components/MovieCard.jsx
@@ -4,6 +4,15 @@ const MovieCard = ({movie:{title, vote_average, poster_path, release_Date, origi
     return(
         <div className='movie-card'>
             <img src={poster_path? `http://image.tmdb.org/t/p/w500/${poster_path}` :'/no-movie.png'} alt="movie poster" />
+
+        <div className='mt-4'>
+            <h3>{title}</h3>
+            <div className='content'>
+                <div className='ratings'>
+                    <img src="star.svg" alt="star Icon" />
+                </div>
+            </div>
+        </div>
         </div>
     )
 };
