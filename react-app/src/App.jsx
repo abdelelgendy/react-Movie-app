535fc08a3784d26dfc5a84273494bcbe82bf4111
Abdelrahman Elgendy <abdelelgendy11@gmail.com>
2025-10-06 00:02:09 -0400

    Update App.jsx

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -55,7 +55,9 @@ const fetchMovies = async (query='') => {
 
     setMovieList(data.results || []);
 
-    updateSearchCount();
+    if (query && data.results.length > 0){
+      await updateSearchCount(query, data.results[0]);
+    }
   } catch (error) {
     console.log(`Error fetching movies: ${error}`);
     setErrorMessage(`Error fetching movies: ${error.message}`);
