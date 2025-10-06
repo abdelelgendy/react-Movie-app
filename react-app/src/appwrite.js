ac160796341a4a1bf5dfd75cb01061d3d95fcadb
Abdelrahman Elgendy <abdelelgendy11@gmail.com>
2025-10-05 23:32:36 -0400

    Update appwrite.js

--- react-app/src/appwrite.js
+++ react-app/src/appwrite.js
@@ -1,4 +1,4 @@
-import { Client, Query } from "appwrite";
+import { Databases, Client,ID,Query } from "appwrite";
 
 const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
 const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
@@ -19,10 +19,21 @@ export const updateSearchCount = async (searchTerm, movie)=>{
         ])
 
         if(result.documents.length >0 ){
-            
+            const doc = result.documents[0];
+         
+            await databse.updateDocument(DATABASE_ID, COLLECTION_ID, doc.$id, {
+                count: doc.count +1,
+            })
+        }else{
+            await databse.createDocument(DATABASE_ID, COLLECTION_ID,ID.unique(),{
+                searchTerm,
+                count: 1,
+                movie_id: movie.id,
+                poster_url: `https://image.tmbd.org/t/p/w500${movie.poster_path}`,
+            })
         }
     }catch (error){
-
+        console.error("Error updating search count:", error);
     }
     //if yes update count
     // else create new search term and couunt for term
